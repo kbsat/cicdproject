19f7426a24b7265e18d874c87196fd73c788445f
BeomsooKim <kbsat@naver.com>
2021-11-28 16:12:19 +0900

    init

---
+++ src/main/java/com/cicdproject/CicdprojectApplication.java
@@ -0,0 +1,13 @@
+package com.cicdproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cicdproject/CicdprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicdproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
